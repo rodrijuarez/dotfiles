ac1dc000a2735f077db7df084de82ea63e63ba39
rodrijuarez <rodrigo.juarez.inf@gmail.com>
2018-10-15 13:51:40 +0200

    Config updates script

---
+++ UpdateConfigs.hs
@@ -0,0 +1,39 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-10.2 script
+{-# LANGUAGE OverloadedStrings #-} --
+                                    --
+
+import Control.Monad (liftM2)
+import Data.Monoid ((<>))
+import Filesystem.Path (filename)
+import Filesystem.Path.CurrentOS (decodeString, encodeString)
+import Prelude hiding (FilePath)
+import System.Directory (getHomeDirectory)
+import Turtle (cp)
+
+main = do
+  copyVimrc
+  copyPluginsVim
+  copyMappingsVim
+  copyKittyConf
+
+dotfiles =
+  (decodeString <$> getHomeDirectory) <> (return $ decodeString "dotfiles/")
+
+fromHome x = (decodeString <$> getHomeDirectory) <> (return $ decodeString x)
+
+fromDotfiles x = (dotfiles <> (return $ decodeString x))
+
+cpDotfile originalPath = do
+  name <- filename <$> originalPath
+  from <- originalPath
+  to <- (fromDotfiles $ encodeString name)
+  cp from to
+
+copyVimrc = cpDotfile (fromHome ".vimrc")
+
+copyPluginsVim = cpDotfile (fromHome ".plugins.vim")
+
+copyMappingsVim = cpDotfile (fromHome ".mappings.vim")
+
+copyKittyConf = cpDotfile (fromHome ".config/kitty/kitty.conf")
