6ba772fb4c95ca6e93e87f22a8716a59f64bd3e2
rodrijuarez <rodrigo.juarez.inf@gmail.com>
2018-10-16 16:49:42 +0200

    Rename script

--- RenameAll.hs
+++ RenameAll.hs
@@ -1,15 +1,29 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-10.2 script
-{-# LANGUAGE OverloadedStrings #-} --
-                                    --
+{-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad (liftM2)
-import Data.Monoid ((<>))
-import Filesystem.Path (filename)
+import Data.Text (pack, replace, unpack)
 import Filesystem.Path.CurrentOS (decodeString, encodeString)
 import Prelude hiding (FilePath)
-import System.Directory (getHomeDirectory)
-import Turtle (FilePath, cp)
+import Turtle
+
+parser :: Parser (Text, Text, FilePath, Int)
+parser =
+  (,,,) <$> argText "from" "From string" <*> argText "to" "To string" <*>
+  argPath "src" "Src directory to have as reference" <*>
+  argInt "print" "Information mode, no replacement"
 
 main = do
-  putStrLn "Hello"
+  (from, to, src, print) <- options "A simple renamer utility" parser
+  sh $ renameAll from to src
+  return ()
+
+renameAll :: Text -> Text -> FilePath -> Shell ()
+renameAll from to src = do
+  file <- find (has (text from)) src
+  renameFileTo from to file
+
+renameFileTo :: Text -> Text -> FilePath -> Shell ()
+renameFileTo from to file = mv file newFile
+  where
+    newFile = decodeString . unpack . replace from to . pack $ encodeString file
