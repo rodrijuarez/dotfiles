7cad069bfcfebed75896e07c1e3733c39f63369e
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2019-04-02 15:50:37 +0200

    session with name cauldron

--- bin/make-cauldron.sh
+++ bin/make-cauldron.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-tmux has-session -t dev
+tmux has-session -t cauldron
 if [ $? != 0 ]
 then
-    tmux new-session -s dev -n "Cauldron" -d
+    tmux new-session -s cauldron -n "Cauldron" -d
 
-    tmux split-window -h -t dev:0
-    tmux split-window -v -t dev:0.1
-    tmux send-keys -t dev:0.0 'j cauldron && yarn storybook' C-m
-    tmux send-keys -t dev:0.1 'j cauldron' C-m
-    tmux send-keys -t dev:0.2 'j cauldron' C-m
+    tmux split-window -h -t cauldron:0
+    tmux split-window -v -t cauldron:0.1
+    tmux send-keys -t cauldron:0.0 'j cauldron && yarn storybook' C-m
+    tmux send-keys -t cauldron:0.1 'j cauldron' C-m
+    tmux send-keys -t cauldron:0.2 'j cauldron' C-m
 
-    tmux new-window -n "Vim" -t dev
-    tmux send-keys -t dev:1.0 'j cauldron && v package.json' C-m
+    tmux new-window -n "Vim" -t cauldron
+    tmux send-keys -t cauldron:1.0 'j cauldron && v package.json' C-m
 
-    tmux new-window -n "Tests" -t dev
-    tmux send-keys -t dev:2.0 'j cauldron && yarn test' C-m
+    tmux new-window -n "Tests" -t cauldron
+    tmux send-keys -t cauldron:2.0 'j cauldron && yarn test' C-m
 
-    tmux select-window -t dev:0
+    tmux select-window -t cauldron:0
 fi
-tmux attach -t dev
+tmux attach -t cauldron
